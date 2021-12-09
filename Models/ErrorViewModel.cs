1d24ea07ed822f9c5d17b6e5b890a190d9a9c62f
JP Tugirimana <tugirimana.jp@gmail.com>
2021-12-09 10:37:11 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UScooter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
